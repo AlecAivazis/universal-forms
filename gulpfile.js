194493dfe13687f00c64719040dfc80883a36bcb
alec aivazis <alec@aivazis.com>
2015-10-07 18:18:34 -0700

    fixed test unit glob

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ const project_dirs = {
  * Run the test suite.
  */
 gulp.task('test', function() {
-    gulp.src(project_dirs.test + '**/*.js', {read: false})
+    gulp.src(project_dirs.test + '**/test_*.js', {read: false})
 
         .pipe(mocha({require: ['chai']}))
 })
